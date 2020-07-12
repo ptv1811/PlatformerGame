398b6cfc9277dec4f901e9f07bbdf9a2eeea4b40
Van <51709517+ptv1811@users.noreply.github.com>
2020-07-12 20:57:23 +0700

    add animation

---
+++ MyFirst/Assets/Script/ParallaxLayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxLayer : MonoBehaviour
+{
+	public float parallaxFactor;
+
+    private void Start()
+    {
+        if (parallaxFactor < 0)
+        {
+            transform.localScale *= 1 + Mathf.Abs(parallaxFactor);
+        }
+    }
+
+    public void Move(float delta)
+    {
+        Vector3 newPos = transform.localPosition;
+        newPos.x -= delta * parallaxFactor;
+        transform.localPosition = newPos;
+    }
+}
